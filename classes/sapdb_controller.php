9822b49aa6d3bd52e98c133bd98195757c14922a
NinaHerrmann <nina.herrmann@uni-muenster.de>
2023-08-02 14:49:58 +0200

    test get_teachers_pid_sap()

--- classes/sapdb_controller.php
+++ classes/sapdb_controller.php
@@ -33,26 +33,51 @@ namespace local_sap;
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-define("SAP_GRUPPE",            "public.ovv_e_title");
-define("SAP_GRUPPE_V",          "public.ovv_e_klvl");
-define("SAP_V_GRUPPE",          "public.ovv_klvl_e");
-define("SAP_GRUPPE_P",          "public.ovv_e_p");
-define("SAP_VERANST",           "public.ovv_klvl_title");
-define("SAP_VERANST_DETAILS",   "public.ovv_klvl_periods");
-define("SAP_VERANST_KOMMENTAR", "public.ovv_klvl_comment");
+define("SAP_GRUPPE",            "ovv_e_title");
+define("SAP_GRUPPE_V",          "ovv_e_klvl");
+define("SAP_V_GRUPPE",          "ovv_klvl_e");
+define("SAP_GRUPPE_P",          "ovv_e_p");
+define("SAP_VERANST",           "ovv_klvl_title");
+define("SAP_VERANST_DETAILS",   "ovv_klvl_periods");
+define("SAP_VERANST_KOMMENTAR", "ovv_klvl_comment");
 define("SAP_PERSONAL",          "ovv_lehrende");
-define("SAP_PERSONAL_LOGIN",    "public.ovv_lehr_email");
-define("SAP_VER_PO",      	"public.ovv_klvl_po");
+define("SAP_PERSONAL_LOGIN",    "ovv_lehr_email");
+define("SAP_VER_PO",      	"ovv_klvl_po");
 
 class sapdb_controller {
 
+    /**
+     * @var \moodle_database|null
+     */
+    private $db;
+
+    /**
+     * Creates the moodle database parameter db.
+     */
+    function __construct() {
+        $this->db = \local_sap\sapdb::get();
+    }
+
     public function test_connection () {
 
-        $db = \local_sap\sapdb::get();
-
         // return $db->get_records_sql("SELECT * FROM " . SAP_PERSONAL . " LIMIT 50", []);
-        return $db->get_records(SAP_PERSONAL,null,"", "vorname, nachname" ,0, 50);
+        return $this->db->get_records(SAP_PERSONAL,null,"", "vorname, nachname" ,0, 50);
 
     }
+
+    /**
+     * get_teachers_pid returns the pid (personen-id) connected to a specific username.
+     *
+     * @param $username string the teachers username
+     * @return $sapid the teachers sapid (personen-id)
+     */
+    function get_teachers_pid_sap($username) {
+        $teacherrecord = $this->db->get_record(SAP_PERSONAL_LOGIN, array('login' => strtoupper($username)), 'sapid');
+        if (!empty($teacherrecord)) {
+            return $teacherrecord->sapid;
+        }
+        return null;
+    }
+
 
 }
